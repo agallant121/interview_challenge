4eb43e88a006ac1cea317abe1a968daeb4fa8c34
Alex Gallant <agallant1201@gmail.com>
2020-09-01 18:23:03 -0600

    Change spelling error for test push

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,7 +8,7 @@ module Types
     field :hello_world, # name of method
           String, # Type the data will be
           null: false, # If it can ever return a null value
-          description: 'An example field added for your reference!' # Optional Descriotion
+          description: 'An example field added for your reference!' # Optional Description
 
     def hello_world
       'Hello World - This is your Interview Challenge!\nGood luck.'
