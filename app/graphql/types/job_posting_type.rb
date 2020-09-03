459fbcc3df26d2fb5e7d0fd803d7521fdc9902e9
Alex Gallant <agallant1201@gmail.com>
2020-09-03 13:55:01 -0600

    Add descriptions of each argument for a job_posting

--- app/graphql/types/job_posting_type.rb
+++ app/graphql/types/job_posting_type.rb
@@ -1,11 +1,25 @@
 module Types
   class JobPostingType < Types::BaseObject
-    field :id, ID, null: false
-    field :title, String, null: true
-    field :company_name, String, null: true
-    field :url, String, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :job_applications, Types::HasManyType, null: true
+    field :id, ID, null: false do
+      description "This JobPosting's ID."
+    end
+    field :title, String, null: true do
+      description "This JobPosting's title."
+    end
+    field :company_name, String, null: true do
+      description "This JobPosting's company's name."
+    end
+    field :url, String, null: true do
+      description "This JobPosting's url."
+    end
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false do
+      description "The date/time this JobPOsting was created at."
+    end
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false do
+      description "The date/time this JobPOsting was updated at."
+    end
+    field :job_applications, [JobApplicationType], null: true do
+      description "This JobPosting's applications."
+    end
   end
 end
