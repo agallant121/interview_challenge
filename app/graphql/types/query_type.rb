60c33d92cd3e699ee365d60f3e6fa28633caa2b3
Alex Gallant <agallant1201@gmail.com>
2020-09-03 13:54:13 -0600

    Add action to query all candidates

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,16 +2,25 @@
 
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    # # Add root-level fields here.
+    # # They will be entry points for queries on your schema.
+    #
+    # field :hello_world, # name of method
+    #       String, # Type the data will be
+    #       null: false, # If it can ever return a null value
+    #       description: 'An example field added for your reference!' # Optional Description
+    #
+    # def hello_world
+    #   'Hello World - This is your Interview Challenge!\nGood luck.'
+    # end
 
-    field :hello_world, # name of method
-          String, # Type the data will be
-          null: false, # If it can ever return a null value
-          description: 'An example field added for your reference!' # Optional Description
-
-    def hello_world
-      'Hello World - This is your Interview Challenge!\nGood luck.'
+    field :allCandidates, [Types::CandidateType], null: false do
+      description "Query that selects all candidates."
     end
+
+    def allCandidates
+      return Candidate.all
+    end
+
   end
 end
