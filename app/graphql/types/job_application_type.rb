bc54243836db94625cf76cd3e375887465b8d135
Alex Gallant <agallant1201@gmail.com>
2020-09-03 10:39:49 -0600

    Add graphql job_application type with references to cadidates and job_postings as attributes

---
+++ app/graphql/types/job_application_type.rb
@@ -0,0 +1,12 @@
+module Types
+  class JobApplicationType < Types::BaseObject
+    field :id, ID, null: false
+    field :candidate_id, Integer, null: false
+    field :job_posting_id, Integer, null: false
+    field :is_active, Boolean, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :candidate, Types::ReferencesType, null: true
+    field :job_posting, Types::ReferencesType, null: true
+  end
+end
