8491eab34720695f1ebea69fe9b9dac6220b9d81
Alex Gallant <agallant1201@gmail.com>
2020-09-03 17:37:00 -0600

    Add method to find the job applications for a specific candidate

--- app/graphql/types/candidate_type.rb
+++ app/graphql/types/candidate_type.rb
@@ -3,23 +3,41 @@ module Types
     field :id, ID, null: false do
       description "This candidate's ID."
     end
+
     field :first_name, String, null: true do
       description "This candidate's first name."
     end
+
     field :last_name, String, null: true do
       description "This candidate's last name."
     end
+
     field :email, String, null: true do
       description "This candidate's email."
     end
+
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false do
       description "The date/time that this candidate was created at."
     end
+
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false do
       description "The date/time that this candidate was last updated at."
     end
-    field :job_application, JobApplicationType, null: true do
+
+    field :job_applications, [JobApplicationType], null: true do
       description "This candidate's job applications."
     end
+
+    def job_applications
+      object.job_applications
+    end
+
+    # field :job_app_count, Integer, null: true do
+    #   description "Count of the apps for a candidate"
+    # end
+    #
+    # def job_app_count
+    #   object.job_applications.count
+    # end
   end
 end
