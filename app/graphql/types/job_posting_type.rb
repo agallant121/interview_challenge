fd0c45beacf54075364756c244f659d3b60e75cc
Alex Gallant <agallant1201@gmail.com>
2020-09-03 10:42:47 -0600

    Remove duplicated information from job_posting type

--- app/graphql/types/job_posting_type.rb
+++ app/graphql/types/job_posting_type.rb
@@ -6,9 +6,6 @@ module Types
     field :url, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :title, String, null: true
-    field :company_name, String, null: true
-    field :url, String, null: true
     field :job_applications, Types::HasManyType, null: true
   end
 end
