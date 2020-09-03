c5f0e80a309a2bc69d8b5e2b58055f49b21b4e96
Alex Gallant <agallant1201@gmail.com>
2020-09-03 10:39:12 -0600

    Add graphql job_posting type with title, company_name and url attributes

---
+++ app/graphql/types/job_posting_type.rb
@@ -0,0 +1,14 @@
+module Types
+  class JobPostingType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :company_name, String, null: true
+    field :url, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :title, String, null: true
+    field :company_name, String, null: true
+    field :url, String, null: true
+    field :job_applications, Types::HasManyType, null: true
+  end
+end
